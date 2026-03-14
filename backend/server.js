be61f5578912ab148afd9af6f8cc4020e02b1a16
Sanjana Janakiraman <sanjanajanakiraman5@gmail.com>
2026-03-14 13:10:19 +0530

    updated server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,8 @@ app.delete("/notes/:id", (req, res) => {
     res.json({ message: "Deleted" });
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
